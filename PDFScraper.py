e2e299dc296789837af1f18116996e9f983322af
PIYUSH <me.piyush.360@gmail.com>
2020-08-24 15:08:10 +0530

    Now we can fetch all 50 Resumes through the script

---
+++ PDFScraper.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import time
+#Script to get 50 the pdfs frome LinkedIn
+
+options = webdriver.ChromeOptions() 
+download_dir = '<DOWNLOAD DIRECTORY>'
+prefs = {"download.default_directory": download_dir,"download.prompt_for_download": False}
+options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome('<CHROME_EXECUTABLE',options=options)
+driver.maximize_window()
+driver.get('https://www.linkedin.com/login')
+
+PAUSE_TIME = 3
+
+Email = '<LINKEDIN EMAIL>'
+Password = '<LINKEDIN PASSWORD>'
+
+EmailInput = driver.find_element_by_xpath('//*[@id="username"]')
+EmailInput.send_keys(Email)
+PasswordInput = driver.find_element_by_xpath('//*[@id="password"]')
+PasswordInput.send_keys(Password)
+SubmitButton = driver.find_element_by_css_selector('button[type="submit"]')
+SubmitButton.click()
+
+time.sleep(PAUSE_TIME)
+driver.execute_script('window.stop()')
+
+ConnectionPageURL = 'https://www.linkedin.com/mynetwork/invite-connect/connections/'
+driver.get(ConnectionPageURL)
+
+
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(PAUSE_TIME)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+
+ProfileLinkElements = driver.find_elements_by_class_name('mn-connection-card__picture')
+AllProfileLinks = [element.get_attribute('href') for element in ProfileLinkElements]
+ProfileLinks = AllProfileLinks[0:50]
+
+#The program is paused in many steps because LinkedIn might block the request which occurs in milliseconds or seems automated
+for Link in ProfileLinks:
+    driver.get(Link)
+    time.sleep(PAUSE_TIME)
+    MoreButton = driver.find_element_by_class_name('pv-s-profile-actions__overflow-toggle')
+    MoreButton.click()
+    time.sleep(1)
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(MoreButton, 0, 85)
+    action.click()
+    action.perform()
+    time.sleep(30)
+    
+driver.quit()
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,23 @@
+# Description
+
+This repository contains scripts and code for four tasks:  
+1. Download 50 public profile PDFs of your connections (randomly) from LinkedIn.  
+2. Extract text from the above PDFs and store them in a CSV.  
+3. For every profile data (text), find out the most frequent words and essential words used. It
+shouldn’t contain stop words (like is, the, an, etc.).  
+4. Create two web APIs using flask/Django or another framework of your choice.
+    * The first web API should take a PDF file as input and return the text in it in JSON
+    format.  
+    * The second web API should take text data as input and return the most frequent
+    words and important words (as mentioned in 3) in JSON format.  
+
+# Files And Directory Structure
+#### PDFScraper.py
+PDF Scraper, obviously, scrapes PDFs of one's LinkedIn connections. Certain variables like username and password needs to be provided by the user. It uses selenium (yes, it is a testing suite but here is being hacked a web scraper ) and Google Chrome.
+
 # Prerequisites
-<li>
-- Selenium
+
+* Selenium<br/>
+* Python 3<br/>
+* Google Chrome<br/>
+ 
